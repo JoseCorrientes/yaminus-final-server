fb86ce7a9863f71bd4fde6175ba508c0f0e8a01e
Jose Ernesto Garcia <josernestogarcia609@gmail.com>
2023-04-07 17:16:13 -0300

    commit3

--- components/products/deleteProduct/deleteProduct.controller.js
+++ components/products/deleteProduct/deleteProduct.controller.js
@@ -1,17 +1,14 @@
-const newPool = require('../../../database/database');
-const sqlText= require('./deleteProduct.sql');
+const newPool = require("../../../database/database");
+const sqlText = require("./deleteProduct.sql");
 
-async function deleteProductController (codigo) {
-    try{
-        const result = await newPool.query(sqlText[0],[codigo]);
-        if (result.rowCount>0) return 200
-        else return 400
-    }catch(e) {
-        return `Error ${e}`
-    } 
+async function deleteProductController(codigo) {
+  try {
+    const result = await newPool.query(sqlText[0], [codigo]);
+    if (result.rowCount > 0) return 200;
+    else return 400;
+  } catch (e) {
+    return `Error ${e}`;
+  }
 }
 
 module.exports = deleteProductController;
-
-
-
